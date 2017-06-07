d28c116370cc3d964399519a861158e77199383c
Geoff Harper <GHarper1@md009901.ad.ypg.com>
2017-06-07 16:30:20 -0700

    show more/less toggle

--- src/components/presentational/BarListItem/index.js
+++ src/components/presentational/BarListItem/index.js
@@ -1,18 +1,46 @@
 import React from 'react'
 
-const BarListItem = ({name, description, brands, address}) => {
-  return (
-    <li>
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <ul>
-        {brands.map((brand, i) =>
-          <li key={i}>{brand.name}</li>
-        )}
-        <li>...see more</li>
-      </ul>
-    </li>
-  )
+class BarListItem extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      showAll: false
+    }
+
+    this.toggleList = this.toggleList.bind(this)
+    this.updateBrands = this.updateBrands.bind(this)
+  }
+
+  toggleList() {
+    (this.state.showAll) ? this.setState({showAll:false}) : this.setState({showAll:true})
+  }
+
+  updateBrands() {
+    if(this.state.showAll) {
+      return (this.props.brands);
+    } else {
+      return (this.props.brands.slice(0,2));
+    }
+  }
+
+  render() {
+    var _brands = this.updateBrands();
+    var prompt = (this.state.showAll) ? "... see less" : "... see more"
+
+    return (
+      <li>
+        <h3>{this.props.name}</h3>
+        <p>{this.props.description}</p>
+        <ul>
+          {_brands.map((brand, i) =>
+            <li key={i}>{brand.name}</li>
+          )}
+          <li onClick={this.toggleList}>{prompt}</li>
+        </ul>
+      </li>
+    )
+  }
 }
 
 export default BarListItem
