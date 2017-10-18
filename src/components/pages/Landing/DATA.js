8d7b1bb74946adc35f9e5831af2318328a5ea174
Geoff Harper <GHarper1@md009901.ad.ypg.com>
2017-10-18 10:50:18 -0700

    Added more bars and fixed small styling snafoos

--- src/components/pages/Landing/DATA.js
+++ src/components/pages/Landing/DATA.js
@@ -119,6 +119,178 @@ const DATA = [
     "latitude": 49.272216,
     "longitude": -123.134332,
     "phone": "(604) 687-1354"
+  },
+  {
+    "id": "4",
+    "name": "Juniper",
+    "brands": [
+      {
+        "name": "33 Acres",
+        "brews": ["white"]
+      },
+      {
+        "name": "Strange Fellows",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Dageraad",
+        "brews": ["amber"]
+      },
+      {
+        "name": "Tod Creek",
+        "brews": ["cider"]
+      }
+    ],
+    "description": "A fresh and vibrant distillation of the Pacific Northwest, 100-seat Juniper Kitchen & Bar is a Cascadian-themed restaurant in the heart of Vancouver’s vibrant Chinatown neighbourhood. We believe good food creates community and we look forward to introducing you to our family.",
+    "address": "185 Keefer Street, Vancouver, BC V6A 1X3",
+    "latitude": 49.279608,
+    "longitude": -123.100241,
+    "phone": "(604) 681-1695"
+  },
+  {
+    "id": "5",
+    "name": "Pourhouse Vancouver",
+    "brands": [
+      {
+        "name": "Four Winds",
+        "brews": ["ipa"]
+      },
+      {
+        "name": "TheGeezer English Porter",
+        "brews": ["english"]
+      },
+      {
+        "name": "33 Acres",
+        "brews": ["white"]
+      },
+      {
+        "name": "Strange Fellows",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Porter Pilsner",
+        "brews": ["pilsner"]
+      }
+    ],
+    "description": "Our beers cover the range of tastes and styles – several selections representing the true ale craftsmanship of bc and the pacific northwest. Serious about coffee; pourhouse serves a beautiful local roast, crafted with a handmade synesso espresso machine – overall, a focus on quality over quantity, with a particular in interest in whiskey and whiskey applications to cocktails. The true heart of bartending is shown through the creation of personalized experiences.",
+    "address": "162 Water St, Vancouver, BC V6B 1B2",
+    "latitude": 49.284039,
+    "longitude": -123.108315,
+    "phone": "(604) 647-2287"
+  },
+  {
+    "id": "6",
+    "name": "Railway Stage and Beer Café",
+    "brands": [
+      {
+        "name": "Parallel 49",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Bomber Park Life Passion Fruit",
+        "brews": ["fruit"]
+      },
+      {
+        "name": "Doan's Ale — Altbier",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Twin Sails Dat Juice",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Off the Rail Belt Up",
+        "brews": ["bitter"]
+      },
+      {
+        "name": "Phillips Kaleidoscope Mosaic",
+        "brews": ["ipa"]
+      },
+      {
+        "name": "Driftwood Fat Tug",
+        "brews": ["ipa"]
+      },
+      {
+        "name": "Vancouver Island 48",
+        "brews": ["lager"]
+      },
+      {
+        "name": "Strathcona Premium Pilsner",
+        "brews": ["pilsner"]
+      },
+      {
+        "name": "Boneyard Backbone Chocolate Espresso",
+        "brews": ["stout"]
+      },
+      {
+        "name": "Hoyne Dark Matter",
+        "brews": ["stout"]
+      },
+      {
+        "name": "Tofino Kelp Stout",
+        "brews": ["stout"]
+      },
+      {
+        "name": "Merridale House Cider",
+        "brews": ["cider"]
+      },
+      {
+        "name": "Sunday Wild Cider",
+        "brews": ["cider"]
+      },
+      {
+        "name": "Postmark",
+        "brews": ["pale", "ipa"]
+      }
+    ],
+    "description": "Long-time stop for pub grub offering many craft & import beers, plus live music & a bohemian vibe.",
+    "address": "579 Dunsmuir Street, Vancouver, BC V6B 1Y4",
+    "latitude": 49.282877,
+    "longitude": -123.115054,
+    "phone": "(604) 564-1430"
+  },
+  {
+    "id": "7",
+    "name": "The Cambie Pub",
+    "brands": [
+      {
+        "name": "Lucky Lager",
+        "brews": ["lager"]
+      },
+      {
+        "name": "Budweiser",
+        "brews": ["lager"]
+      },
+      {
+        "name": "Kokanee",
+        "brews": ["lager"]
+      },
+      {
+        "name": "Parallel 49",
+        "brews": ["lager", "amber"]
+      },
+      {
+        "name": "Stanley Park",
+        "brews": ["pilsner", "pale", "ipa"]
+      },
+      {
+        "name": "Shock Top",
+        "brews": ["white"]
+      },
+      {
+        "name": "Moosehead",
+        "brews": ["pale"]
+      },
+      {
+        "name": "Whistler",
+        "brews": ["honey", "english"]
+      }
+    ],
+    "description": "The Cambie is Vancouver’s oldest bar. The Cambie’s building was re-built soon after Vancouver was burnt to the ground in 1886. The establishment has held a special Class A Liquor license since 1897, which grandfathered extra benefits over to its current liquor license. 2017 marks The Cambie’s 120th anniversary of serving Vancouverites and tourists alike!",
+    "address": "300 Cambie St, Vancouver, BC V6B 2N3",
+    "latitude": 49.283121,
+    "longitude": -123.108989,
+    "phone": "(604) 684-6466"
   }
 ]
 export default DATA
